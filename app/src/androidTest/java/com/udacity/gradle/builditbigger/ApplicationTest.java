def25210b3a8b336cf9a4ede3e46f3816a53fa62
Yin Zheng <yinzheng@gmail.com>
2017-02-01 12:17:25 +1100

    implemented: Step 4: Add Functional Tests

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -0,0 +1,42 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.support.v4.util.Pair;
+import android.test.InstrumentationTestCase;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Iris on 31/01/2017.
+ * */
+public class ApplicationTest extends InstrumentationTestCase {
+    public void testAsyncTask () throws Throwable {
+        // create  a signal to let us know when our task is done.
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        final EndpointsAsyncTask myTask
+                = new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                assertFalse(result, equals(""));
+                signal.countDown();
+                }
+        };
+
+        // Execute the async task on the UI thread! THIS IS KEY!
+        runTestOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                final MainActivity mainActivity = new MainActivity();
+                myTask.execute(new Pair<Context, String>(mainActivity, ""));
+            }
+        });
+
+        /* The testing thread will wait here until the UI thread releases it
+     * above with the countDown() or 30 seconds passes and it times out.
+     */
+        signal.await(30, TimeUnit.SECONDS);
+    }
+}
